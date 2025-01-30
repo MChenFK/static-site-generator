556f9af6ee7f7e943b0fff8c83491da25603802a
MChenFK <michaelchen1923@gmail.com>
2025-01-30 05:25:43 -0500

    added node splitting for images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -29,3 +29,61 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        matches = extract_markdown_images(original_text)
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue
+        for match in matches:
+            sections = original_text.split(f"![{match[0]}]({match[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown syntax: image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    match[0],
+                    TextType.IMAGE,
+                    match[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        matches = extract_markdown_links(original_text)
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue
+        for match in matches:
+            sections = original_text.split(f"[{match[0]}]({match[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown syntax: link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    match[0],
+                    TextType.LINK,
+                    match[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
