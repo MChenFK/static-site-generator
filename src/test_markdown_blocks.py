dd561d9e6cf36cfbe6864c9f0b9568972a828f2f
MChenFK <michaelchen1923@gmail.com>
2025-01-31 03:19:15 -0500

    added function markdown_to_blocks to split markdown format into blocks of text

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,78 @@
+import unittest
+from markdown_blocks import markdown_to_blocks
+
+
+class TestMarkdownToHTML(unittest.TestCase):
+    # Test markdown_to_blocks
+    def test_markdown_to_blocks_various_sizes_and_newlines(self):
+        md = """
+A
+
+
+
+
+A
+A
+
+
+
+
+* A
+* A
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "A",
+                "A\nA",
+                "* A\n* A",
+            ],
+        )
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
