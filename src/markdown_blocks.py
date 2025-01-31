30b5a35fcb67b2c8e12241f4fbae841f58190f2c
MChenFK <michaelchen1923@gmail.com>
2025-01-31 06:09:04 -0500

    added block to block type conversion for headings and code

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,10 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     new_blocks = []
@@ -7,3 +14,14 @@ def markdown_to_blocks(markdown):
             continue
         new_blocks.append(block)
     return new_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if "# " in block[:7]:
+    #if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+    if block[:3] == "```" and block [-3:] == "```":
+        return block_type_code
+    
+    return "paragraph"
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,14 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_quote,
+    block_type_unordered_list,
+    block_type_ordered_list,
+)
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -72,7 +81,39 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
-
+    
+    # Test block_to_block_types:
+    def test_block_to_block_types(self):
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        # block = "```\ncode\n```"
+        # self.assertEqual(block_to_block_type(block), block_type_code)
+        # block = "> quote\n> more quote"
+        # self.assertEqual(block_to_block_type(block), block_type_quote)
+        # block = "* list\n* items"
+        # self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+        # block = "1. list\n2. items"
+        # self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+        
+    def test_multiple_heading_layers(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "## heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "### heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "#### heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "##### heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "###### heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+    
+    def test_too_many_heading_layers(self):
+        block = "####### heading"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
 
 if __name__ == "__main__":
     unittest.main()
