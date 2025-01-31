025b08308e5450b51a7a2e0c359786d52737c013
MChenFK <michaelchen1923@gmail.com>
2025-01-31 03:08:17 -0500

    added function to convert text to text nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -87,3 +87,11 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_image(new_nodes)
+    return split_nodes_link(new_nodes)
